d11b2cc3ed2f8d6158cea8e84a63cac41cf42a7c
naomi <naomimanabe3.0@gmail.com>
2024-07-23 16:50:21 -0400

    el primer commit

---
+++ javascripts/validation.js
@@ -0,0 +1,36 @@
+
+function emailValidation() {
+    const form = document.getElementById('form');
+    const emailEscrito = document.getElementById('email');
+    const emailIncorrecto = document.getElementById('email_confirm');
+    const error = document.getElementById('error-row');
+    const Message = document.getElementById('error-message');
+
+    emailIncorrecto.addEventListener('input', () => {
+        if (emailEscrito.value !== emailIncorrecto.value) {
+          Message.textContent = "Eメールが一致しません";
+          error.style.display = 'table-row';
+          emailIncorrecto.classList.add('email_error');
+        } else {
+          Message.textContent = "";
+          error.style.display = 'none';
+          emailIncorrecto.classList.remove('email_error');
+        }
+    });
+    
+      form.addEventListener('submit', e => {
+        if (emailEscrito.value !== emailIncorrecto.value) {
+          e.preventDefault();
+          Message.textContent = "Eメールが一致しません";
+          error.style.display = 'table-row';
+          emailIncorrecto.classList.add('email_error');
+        } else {
+          Message.textContent = "";
+          error.style.display = 'none';
+          emailIncorrecto.classList.remove('email_error');
+          form.submit();
+        }
+    });
+};
+    
+    window.onload = emailValidation;  
